f53ecb9b754bd485600ab37f7deb10731588a631
Munna Das <dasmrpmunna93411@gmail.com>
2026-03-12 11:11:18 +0545

    Add New

--- Day2/PHP_Constants.php
+++ Day2/PHP_Constants.php
@@ -28,6 +28,56 @@
     myTest();
     echo Aryan;
 
+    echo "<br>";
+    define ("Ben", "Welcome to my Home..🏠");
+    echo Ben;
+
+    echo"<br>";
+    function myHome(){
+        define("Mahi", "Thanks Ben");
+    }
+    myHome();
+    echo Mahi;
+
+    echo "<h2>PHP const Keyword</h2>";
+    echo "<h3>The const keyword defines a constant at compile-time.</h3>";
+    // Constants defined with the const keyword must be declared at the top-level scope because they are defined at compile-time. This means that they cannot be declared inside functions, loops, if/else statements or try/catch blocks.
+    // Constants defined with the const keyword can be used to define class constants.
+    // Constants defined with the const keyword are case-sensitive.
+    // Syntxa
+    // const CONSTANT_NAME = value;
+
+    // Example
+    // echoCreate a constant named "GREETING" with const:
+
+    const Hello = "Hello Aryan., How are you.?";
+    echo Hello;
+
+    echo "<h3>Exmaple of using Define and Const</h3>";
+    function Communication(){
+        define("Greeting","Hello Aryan, How are you..?");
+    }
+    Communication();
+    echo Greeting;
+
+    echo"<br>";
+    
+    const Communication = "I'm good Mahi, How about you..?";
+    function Greeting(){
+        echo Communication;
+    }
+    Greeting();
+    echo"<br>";
+    echo"<br>";
+
+    //using define in Array
+    define("Car",array("Jeep","Valvo","Toyato","BMW"));
+    echo Car[0];
+    echo"<br>";echo"<br>";
+    //using Const in Array;
+    const Animals = array("Dog","Cat","Elephant","Lion");
+    echo Animals[1];
+
     ?>
 </body>
 </html>
