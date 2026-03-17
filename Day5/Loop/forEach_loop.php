95d113b083074447a5a65d7e394a68d8d2a5e207
Munna Das <dasmrpmunna93411@gmail.com>
2026-03-17 12:19:03 +0545

    Add forEach Loop

--- Day5/Loop/forEach_loop.php
+++ Day5/Loop/forEach_loop.php
@@ -21,7 +21,71 @@
    
     echo "<h3> PHP foreach Loop on Associative Arrays </h3>";
 
+    $member = array("Peter" => "21", "Ben" => "23", "Joe" => "20");
+    foreach($member as $key => $value){
+        echo "$key : $value <br>";
+    }
 
+    echo "<br>";echo "<br>";
+    ##############################################################
+   
+    echo "<h3> PHP foreach Loop on Objects </h3>";
+
+    class myCar{
+        public $color;
+        public $model;
+        public function __construct($color, $model){
+            $this-> color = $color;
+            $this-> model = $model;
+        }
+    }
+    $thisCar = new myCar("White", "Toyato");
+    foreach ($thisCar as $key => $value){
+        echo "$key : $value <br> ";
+    }
+
+    echo "<br>";echo "<br>";
+    ##############################################################
+   
+    echo "<h3> The PHP break Statement </h3>";
+
+    $color = array("red", "blue", "yellow","green","pink");
+    foreach ($color as $x) {
+        if ($x == "yellow") break;
+        echo "$x <br>";
+    }
+
+    echo "<br>";echo "<br>";
+    ##############################################################
+   
+    echo "<h3> The PHP continue Statement </h3>";
+    $car = array("Jeep", "Toyato", "Valvo","Tata", "BMW");
+    foreach($car as $y){
+        if ($y =="Valvo") continue;
+        echo "$y <br>";
+    }
+
+    echo "<br>";echo "<br>";
+    ##############################################################
+   
+    echo "<h3> PHP Foreach Loop Byref </h3>";
+
+    $color = array("red", "blue", "yellow","green");
+    foreach($color as $m){
+        if ($m == "Blue") $m = "pink";
+    }
+    var_dump ($color);
+
+    echo "<br>";echo "<br>";
+    ##############################################################
+   
+    echo "<h3> Alternative Syntax </h3>";
+    echo "<h5> The foreach loop syntax can also be written with the endforeach statement like this </h5>";
+
+    $color = array("red", "blue","yellow","pink","green");
+    foreach($color as $x):
+        echo "$x <br>";
+    endforeach;
     ?>
 
     
